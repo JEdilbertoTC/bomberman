6cd76ad34a7cbfd6930dab0e1695ea789369d0b8
Jocelyn Llamas <70532285+JocelynLlamas@users.noreply.github.com>
2020-11-30 15:10:25 -0600

    HUD dentro del juego (#8)
    * HUD dentro del juego
    
    * HUD final

--- Hud.java
+++ Hud.java
@@ -1,21 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Hud extends Actor{
-    
-    private GreenfootImage sprites[];
+
+    private GreenfootImage sprites[][];
     public Hud(){
-        sprites = new GreenfootImage[4];
-        sprites[0] = new GreenfootImage("images/HUD/HUD0.png");
-        sprites[1] = new GreenfootImage("images/HUD/HUD1.png");
-        sprites[2] = new GreenfootImage("images/HUD/HUD2.png");
-        sprites[3] = new GreenfootImage("images/HUD/HUD3.png");
+        int N=4;
+        sprites = new GreenfootImage[N][N];
+        int i,j;
+
+        for(i=0;i<N;i++){
+            for(j=0;j<N;j++){
+                sprites[i][j] = new GreenfootImage("images/HUD/HUD"+i+"-"+j+".png");
+            }
+        }
     }
-    
+
     public void act(){
         hudStatus();
     }
-    
+
     public void hudStatus(){
-        setImage(sprites[Bomberman.getVidas()]);
+        setImage(sprites[Bomberman.getVidas()][Bomberman.getBombas()]);
     }
+
 }
